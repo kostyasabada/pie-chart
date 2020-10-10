5a9cc04e0296609884dbb60e0be48bcb716a8394
Kostya Sabada <kostyasabada@gmail.com>
2020-10-10 15:01:30 +0300

    Updates

---
+++ precache-manifest.d914a1db100246d4f1577527bfccebc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958122391de04f4b70421996fde4e698",
+    "url": "/pie-chart/index.html"
+  },
+  {
+    "revision": "0e456e3a96162df58643",
+    "url": "/pie-chart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "832ea629451c94272438",
+    "url": "/pie-chart/static/js/2.f7c4be23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pie-chart/static/js/2.f7c4be23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e456e3a96162df58643",
+    "url": "/pie-chart/static/js/main.3ee69cf6.chunk.js"
+  },
+  {
+    "revision": "edc5f7056ed27d9c6b5e",
+    "url": "/pie-chart/static/js/runtime-main.651f6f9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pie-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
